57399b911e43d1168c09ad5b4e336f3b6f151f71
SimpleStone1 <danik.zaizev@mail.ru>
2026-03-09 14:51:23 +0300

    update function added

--- bd.py
+++ bd.py
@@ -4,6 +4,7 @@ class bd:
     def __init__(self):
         self.db_name = '3tour.db'
         self.rectangles = {}
+        self.table = ['ID', 'X1', 'X2', 'Y1', 'Y2']
     
     def create(self):
         try:
@@ -55,6 +56,37 @@ class bd:
         except Exception as e:
             print(f'Ошибка БД {e}')
 
+    def update(self, id, x1=None, x2=None, y1=None, y2=None):
+        try:
+            with sq.connect(self.db_name) as con:
+                cur = con.cursor()
+                
+                updates = []
+                params = []
+                
+                if x1 is not None:
+                    updates.append("X1 = ?")
+                    params.append(x1)
+                if x2 is not None:
+                    updates.append("X2 = ?")
+                    params.append(x2)
+                if y1 is not None:
+                    updates.append("Y1 = ?")
+                    params.append(y1)
+                if y2 is not None:
+                    updates.append("Y2 = ?")
+                    params.append(y2)
+                
+                if not updates:
+                    print("Нет данных для обновления")
+                    return
+                query = f"UPDATE RECTANGLES SET {', '.join(updates)} WHERE ID = ?"
+                params.append(id)
+                
+                cur.execute(query, params)
+                con.commit()
+        except Exception as e:
+            print(f'Ошибка БД при обновлении: {e}')
 if __name__ == '__main__':
     bd1 = bd()
     bd1.create()
